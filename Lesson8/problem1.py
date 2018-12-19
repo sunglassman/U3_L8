19b046ee229b176e4d2da917cb0bb8a8ba6b7b31
sunglassman <43784491+sunglassman@users.noreply.github.com>
2018-12-19 12:05:35 -0500

    Add files via upload

---
+++ Lesson8/problem1.py
@@ -0,0 +1,20 @@
+print('-' * 60)
+print('Morbid Predictions Program: ')
+print()
+
+print('Description: This program asks you for your current age and tells you the year that you will die (on average).')
+print()
+
+age = input('What is your age? ')
+age = int(age)
+yearstodeath = 79 - age
+yearofdeath = 2018 + yearstodeath
+
+print('...thinking...')
+print('...thinking...')
+print('...thinking...')
+ 
+yearofdeath = str(yearofdeath)
+print('You will die in the year... ' + yearofdeath)
+print('-' * 60)
+
